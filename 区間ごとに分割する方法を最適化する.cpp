1821fbf84923bf416115b1471a282f0de9650dd0
YokoPhys-h <yoko.haruru.phy@outlook.jp>
2021-04-01 19:54:25 +0900

    update

---
+++ 区間ごとに分割する方法を最適化する.cpp
@@ -0,0 +1,38 @@
+/**
+*    author:  YokoPhys
+*    created: 2021/04/01 18:14:58
+**/
+
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <typeinfo>
+#include <algorithm>
+#include <set>
+#include <numeric>
+#include <list>
+using namespace std;
+
+template <class T>
+void chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+    }
+}
+
+const long long INF = 1LL << 60;
+
+int main()
+{
+
+    return 0;
+}
